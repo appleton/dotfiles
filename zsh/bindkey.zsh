c30b64aaaa67f9955accea5d6a3e5f0b1abd36ac
Andrew Appleton <andrew@mintdigital.com>
2012-05-25 09:07:58 +0100

    Mac-like text navigation

--- zsh/bindkey.zsh
+++ zsh/bindkey.zsh
@@ -2,3 +2,21 @@
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search
 
+
+### These require the appropriate keybindings to be set up in the terminal
+
+# Skip forward/back a word with opt-arrow
+bindkey '[C' forward-word
+bindkey '[D' backward-word
+
+# Skip to start/end of line with cmd-arrow
+bindkey '[E' beginning-of-line
+bindkey '[F' end-of-line
+
+# Delete word with opt-backspace/opt-delete
+bindkey '[G' backward-kill-word
+bindkey '[H' kill-word
+
+# Delete line with cmd-backspace
+bindkey '[I' kill-whole-line
+
