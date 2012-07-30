88a8d746286934eecc752a18637891ebce8a194a
Andrew Appleton <andrew@mintdigital.com>
2012-07-30 08:19:09 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -8,10 +8,11 @@ do
   echo ">> symlinked ~/.dotfiles/$file to ~/.$file"
 done
 
-for file in Preferences.sublime-settings Default\ \(OSX\).sublime-keymap
-do
-  ln -s ~/.dotfiles/sublime/$file ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/$file
-  echo ">> symlinked ~/.dotfiles/sublime/$file ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/$file"
-done
+ln -s ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+echo ">> symlinked ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings"
+
+ln -s ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
+echo ">> symlinked ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap"
+
 
 echo "\n>> Done! Don't forget to reload your shell."
