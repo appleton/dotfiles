f547db39e98e7953c343e7ecc4066df10c7c5f66
Andrew Appleton <andrew@mintdigital.com>
2012-08-24 09:51:08 +0100

    Add settings for markdown

--- install.sh
+++ install.sh
@@ -11,6 +11,9 @@ done
 ln -s ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
 echo ">> symlinked ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings"
 
+ln -s ~/.dotfiles/sublime/Markdown.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Markdown.sublime-settings
+echo ">> symlinked ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings"
+
 ln -s ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
 echo ">> symlinked ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap"
 
