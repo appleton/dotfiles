7ce5dce3ac6982b2c8040a9066f74a712df9f775
Andrew Appleton <andrew@floatleft.com>
2013-01-07 12:05:53 +0000

    correct path order

--- zsh/rbenv.zsh
+++ zsh/rbenv.zsh
@@ -1,5 +1,5 @@
 # Add rbenv directory to PATH
 export PATH=$HOME/.rbenv/bin:$PATH
+eval "$(rbenv init -)"
 # Add binstubs directory to PATH to avoid bundle execing everything
 export PATH=.bundle/binstubs:$PATH
-eval "$(rbenv init -)"
