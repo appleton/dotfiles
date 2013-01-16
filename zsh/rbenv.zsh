d69df6ceb708ace60d24ddcb2b5e90a5bdefc863
Andrew Appleton <andrew@floatleft.com>
2013-01-16 18:41:25 +0000

    Modify rbenv PATH entry

--- zsh/rbenv.zsh
+++ zsh/rbenv.zsh
@@ -1,5 +1,6 @@
 # Add rbenv directory to PATH
 export PATH=$HOME/.rbenv/bin:$PATH
 eval "$(rbenv init -)"
+export PATH=~/.rbenv/versions:$PATH
 # Add binstubs directory to PATH to avoid bundle execing everything
 export PATH=.bundle/binstubs:$PATH
