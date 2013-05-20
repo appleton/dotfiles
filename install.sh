eb0ad97ee2e1dfcfaeec93a2a67b00f607d7140b
Andrew Appleton <andrew@floatleft.com>
2013-05-20 18:14:01 +0100

    Install for ST3

--- install.sh
+++ install.sh
@@ -11,11 +11,11 @@ done
 for path in ~/.dotfiles/sublime/*
 do
   file=$(basename "$path")
-  ln -s $path ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/$file
-  echo ">> symlinked $path to ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/$file"
+  ln -s $path ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/$file
+  echo ">> symlinked $path to ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/$file"
 done
 
 # Hackety hack. TODO: this properly.
-ln -s ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
+ln -s ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
 
 echo "\n>> Done! Don't forget to reload your shell."
