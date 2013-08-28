9065bee8b3d2cce135a8bb4e295f853f9e45c1f9
Andrew Appleton <andrew@floatleft.com>
2013-08-28 15:10:10 +0100

    Simplify install script

--- install.sh
+++ install.sh
@@ -8,14 +8,6 @@ do
   echo ">> symlinked ~/.dotfiles/$file to ~/.$file"
 done
 
-for path in ~/.dotfiles/sublime/*
-do
-  file=$(basename "$path")
-  ln -s $path ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/$file
-  echo ">> symlinked $path to ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/$file"
-done
-
-# Hackety hack. TODO: this properly.
-ln -s ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
+ln -s ~/.dotfiles/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
 
 echo "\n>> Done! Don't forget to reload your shell."
