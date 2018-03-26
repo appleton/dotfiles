805e664b6b8a2a6db8ca9265a282cafade640b51
Andrew Appleton <appleton@github.com>
2018-03-26 10:32:17 +0100

    Bump hyper config

--- hyper.js
+++ hyper.js
@@ -1,58 +1,124 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
 module.exports = {
   config: {
+    // Choose either "stable" for receiving highly polished,
+    // or "canary" for less polished but more frequent updates
+    updateChannel: 'stable',
+
     // default font size in pixels for all tabs
     fontSize: 13,
 
     // font family with optional fallbacks
-    fontFamily: 'Inconsolata-dz for powerline',
+    fontFamily: 'Inconsolata-dz for Powerline',
 
-    // terminal cursor background color (hex)
-    cursorColor: '#F81CE5',
+    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    cursorColor: '#abb2bf',
+
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    cursorShape: 'BEAM',
+
+    // set to true for blinking cursor
+    cursorBlink: false,
 
     // color of the text
-    foregroundColor: '#fff',
+    foregroundColor: '#abb2bf',
 
     // terminal background color
-    backgroundColor: 'rgba(39,41,44, 0.8)',
+    backgroundColor: '#002833',
 
     // border color (window, tabs)
-    borderColor: '#555',
+    borderColor: '#abb2bf',
 
     // custom css to embed in the main window
-    css: '',
+    css: `
+      .splitpane_pane .term_fit.term_active {
+        filter: brightness(130%);
+      }
+
+      .splitpane_pane .term_fit {
+        filter: brightness(80%);
+      }
+    `,
 
     // custom css to embed in the terminal window
     termCSS: `
       .cursor-node {
-        border: none !important;
-        width: 1px !important;
-        background-color: rgba(255, 255, 255, .6) !important;
+        border-width: 0 0 0 1px !important;
       }
     `,
+
+    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
+    // default: `false` on Linux, `true` on Windows (ignored on macOS)
+    showHamburgerMenu: '',
+
+    // set to `false` if you want to hide the minimize, maximize and close buttons
+    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
+    // default: `true` on windows and Linux (ignored on macOS)
+    showWindowControls: '',
 
     // custom padding (css format, i.e.: `top right bottom left`)
     padding: '12px 14px',
 
-    // some color overrides. see http://bit.ly/29k1iU2 for
-    // the full list
-    colors: [
-      '#000000',
-      '#ff0000',
-      '#33ff00',
-      '#ffff00',
-      '#0066ff',
-      '#cc00ff',
-      '#00ffff',
-      '#d0d0d0',
-      '#808080',
-      '#ff0000',
-      '#33ff00',
-      '#ffff00',
-      '#0066ff',
-      '#cc00ff',
-      '#00ffff',
-      '#ffffff'
-    ]
+    // the full list. if you're going to provide the full color palette,
+    // including the 6 x 6 color cubes and the grayscale map, just provide
+    // an array here instead of a color map object
+    colors: {
+      black: '#282c34',
+      red: '#e06c75',
+      green: '#98c379',
+      yellow: '#e5c07b',
+      blue: '#61afef',
+      magenta: '#c678dd',
+      cyan: '#56b6c2',
+      white: '#abb2bf',
+      lightBlack: '#313640',
+      lightRed: '#fa7883',
+      lightGreen: '#abdb88',
+      lightYellow: '#ffd68a',
+      lightBlue: '#69bbff',
+      lightMagenta: '#dd86f7',
+      lightCyan: '#60cddb',
+      lightWhite: '#b1b9cc'
+    },
+
+    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+    // if left empty, your system's login shell will be used by default
+    //
+    // Windows
+    // - Make sure to use a full path if the binary name doesn't work
+    // - Remove `--login` in shellArgs
+    //
+    // Bash on Windows
+    // - Example: `C:\\Windows\\System32\\bash.exe`
+    //
+    // Powershell on Windows
+    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
+    shell: '',
+
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
+    shellArgs: ['--login'],
+
+    // for environment variables
+    env: {},
+
+    // set to false for no bell
+    bell: 'false',
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: false
+
+    // if true, on right click selected text will be copied or pasted if no
+    // selection is present (true by default on Windows)
+    // quickEdit: true
+
+    // URL to custom bell
+    // bellSoundURL: 'http://example.com/bell.mp3',
+
+    // for advanced config flags please refer to https://hyper.is/#cfg
   },
 
   // a list of plugins to fetch and install from npm
@@ -61,15 +127,16 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [
-    'hyperterm-snazzy',
-    'hyperterm-1password',
-    'hyperterm-title',
-    'hyper-simple-highlight-active-session'
-  ],
+  plugins: ['hyper-tabs-enhanced'],
 
   // in development, you can create a directory under
-  // `~/.hyperterm_plugins/local/` and include it here
+  // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: []
+  localPlugins: [],
+
+  keymaps: {
+    // Example
+    // 'window:devtools': 'cmd+alt+o',
+    // 'editor:deleteBeginningLine': 'command+backspace'
+  }
 };
