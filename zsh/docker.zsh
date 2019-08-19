034687a87e376ed8b16a4ee805093d786254361d
Andrew Appleton <aappleton@plaid.com>
2019-08-19 17:30:37 +0100

    docker logs shortcut

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,2 +1,6 @@
 # eval "$(docker-machine env default &> /dev/null)"
 # sudo route add -net 192.168.99.0/24 -interface vboxnet0
+
+function dlogs {
+  docker ps | grep $1 | cut -f 1 -d " " | xargs docker logs "${@:2}"
+}
